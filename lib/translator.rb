ae1d29045fc90045c78bf66170459c3cd6543d9b
Tobie Tsuzuki <tobie.tsuzuki@gmail.com>
2019-09-24 18:26:40 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,9 +7,9 @@ def load_library (file)
   emoticon_hash.each do |meaning, emoticon|
     make_keys[:get_meaning][emoticon[1]] = meaning
     make_keys[:get_emoticon][emoticon[0] = emoticon[1]
-    binding.pry
   end
   make_keys
+    binding.pry
 end
 
 def get_japanese_emoticon
