93f6ece0b0e3380d7ec571d419fda2ce6d9e5230
Tobie Tsuzuki <tobie.tsuzuki@gmail.com>
2019-09-24 18:10:52 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,9 +2,9 @@ require 'yaml'
 require 'pry'
 def load_library (file)
   emoticon_hash = YAML.load_file(file)
-  emoticon_hash.each do |meaning, emoticon|
+  emoticon_hash.each do |meaning|
     emoticon_hash[:get_meaning] << meaning
-  binding.pry
+    binding.pry
   end
 end
 
