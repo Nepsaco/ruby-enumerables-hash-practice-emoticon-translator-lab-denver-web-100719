1d010f5e4be588cb212838870e0c818f6138041f
Tobie Tsuzuki <tobie.tsuzuki@gmail.com>
2019-09-24 18:18:34 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,7 +2,13 @@ require 'yaml'
 require 'pry'
 def load_library (file)
   emoticon_hash = YAML.load_file(file)
-  emoticon_hash.each do |meaning|
+  emoticon_hash.each do |meaning, emoticon|
+    if emoticon_hash[:get_meaning] == NIL
+      emoticon_hash[:get_meaning] = {}
+    end
+    if emoticon_hash[:get_meaning][:get_emoticon] == NIL
+      emoticon_hash[:get_meaning][:get_emoticon] = []
+    end
     emoticon_hash[:get_meaning].push(meaning)
   end
   emoticon_hash
